4eb2098e07e2e3b331afe1721bd7d16f74905bac
Arlly <arimoto@queserser.co.jp>
2019-05-01 16:37:53 +0900

    JWTの下準備を行った。

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', 'AuthController@login');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/me', 'AuthController@me');
+    Route::post('/logout', 'AuthController@logout');
+});
