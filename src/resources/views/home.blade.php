28d5ce7ff399d14361a748f9e822b8ead62dc23c
Arlly <arimoto@queserser.co.jp>
2019-05-01 17:34:27 +0900

    表示を調整した。

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -8,16 +8,13 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div id="app">
+                        <router-view></router-view>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script src="{{ mix('/js/app.js') }}"></script>
 @endsection
